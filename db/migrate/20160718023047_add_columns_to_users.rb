dbbf5889928a36db59366701611ed78299e1b6fb
Martin Safer <mtsafer@gmail.com>
2016-07-18 11:32:15 -0700

    fix typo

--- db/migrate/20160718023047_add_columns_to_users.rb
+++ db/migrate/20160718023047_add_columns_to_users.rb
@@ -1,6 +1,6 @@
 class AddColumnsToUsers < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :name, :sting
+    add_column :users, :name, :string
     add_column :users, :email, :string
     add_column :users, :password_digest, :string
   end
--- db/schema.rb
+++ db/schema.rb
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 20160718032016) do
   create_table "users", force: :cascade do |t|
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-    t.         "name"
+    t.string   "name"
     t.string   "email"
     t.string   "password_digest"
     t.string   "remember_digest"
