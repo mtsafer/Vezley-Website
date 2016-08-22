7b54e90cdffe82854544a58de3637c7d08fbe4e9
Martin Safer <mtsafer@gmail.com>
2016-08-22 13:22:34 -0700

    fix for deploy

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitch, "h64fm07khcrakk19z884lrp70w7x4de", "cbj4y40xwty392d2ltxe048nv3b7p0y"
+  provider :twitch, "h64fm07khcrakk19z884lrp70w7x4de", "cbj4y40xwty392d2ltxe048nv3b7p0y", {:provider_ignores_state => true}
 end
