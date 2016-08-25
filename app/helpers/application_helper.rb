9680c9d5b7ae9ad678be2be1cb61db15c7b0d141
Martin Safer <mtsafer@gmail.com>
2016-08-25 10:35:55 -0700

    dynamic tab logo and user pic

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,12 @@ module ApplicationHelper
 		base = "Vezzelution"
 		full_title = page.empty? ? base : "#{page} | #{base}"
 	end
+
+	def vezley_logo
+		if User.find_by(name: "vezley")
+			User.find_by(name: "Vezley").logo || "https://static-cdn.jtvnw.net/jtv_user_pictures/vezley-profile_image-91f07216710aec58-300x300.jpeg"
+		else
+			"https://static-cdn.jtvnw.net/jtv_user_pictures/vezley-profile_image-91f07216710aec58-300x300.jpeg"
+		end
+	end
 end
