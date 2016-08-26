9649124f4c75759edd2e1cca5b47268bc691524c
Martin Safer <mtsafer@gmail.com>
2016-08-26 15:49:51 -0700

    maybe fix the login error

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   before_create :create_activation_digest
   before_update :allow_or_deny_custom_status
   validates :custom_status, length: { maximum: 15 },
-                        uniqueness: { case_sensitive: false }
+                        uniqueness: { case_sensitive: false }, :allow_nil
 	#before_save { email.downcase! }
 
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
